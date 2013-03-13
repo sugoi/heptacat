f3fffebf432bb20ba20564e2dc3c3ff132e2386a
Takayuki Muranushi <muranushi@gmail.com>
2013-03-13 10:12:07 +0900

    updating lens test.

--- Heptacat/Type/IO.hs
+++ Heptacat/Type/IO.hs
@@ -1,10 +1,19 @@
 module Heptacat.Type.IO where
 
+
 import qualified Control.Lens as L
 import qualified Control.Monad.State as S
+import qualified Control.Monad.State.Class as S
+import           Data.Default
 import           Heptacat.Type
 
 
+type LaboIO = S.StateT Laboratory IO
 
 getLaboratory :: IO Laboratory
-getLaboratory = return undefined
+getLaboratory = S.execStateT ioLabo def 
+
+ioLabo :: LaboIO Laboratory
+ioLabo = do
+  L.assign (subjectRepo . url) "git@github.com/jk/test"
+  S.get
--- src/heptacat.hs
+++ src/heptacat.hs
@@ -1,7 +1,8 @@
 import Heptacat.Options 
-
+import Heptacat.Type.IO
 
 main = do
   print $ myOptions
-
+  lab <- getLaboratory
+  print lab
 
