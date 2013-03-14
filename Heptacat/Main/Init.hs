1b4283853692cdcddfd133a73299a4c9777ad411
Takayuki Muranushi <muranushi@gmail.com>
2013-03-14 16:48:03 +0900

    create worker!

--- Heptacat/Main/Init.hs
+++ Heptacat/Main/Init.hs
@@ -1,9 +1,13 @@
 module Heptacat.Main.Init where
 
+import           Control.Lens ((^.))
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.Yaml as Yaml
 import           System.IO
+import           System.Directory 
+  (createDirectoryIfMissing)
 
+import Heptacat.Type
 import Heptacat.Type.IO
 
 main :: IO ()
@@ -11,5 +15,10 @@ main = do
   proj <- getProject
   withFile "project.yml" WriteMode $ \h ->
     BS.hPutStrLn h $ Yaml.encode proj
-  
+  let mkdirP = createDirectoryIfMissing True 
+  mkdirP $ proj ^. recordRepo.taskListDir
+  mkdirP $ proj ^. recordRepo.workerStateDir
+  mkdirP $ proj ^. recordRepo.resultDir
+
+
 
---
+++ Heptacat/Main/Worker.hs
@@ -0,0 +1,1 @@
+module Heptacat.Main.Worker where
