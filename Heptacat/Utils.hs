7c0229258805545f3ed8689fec4c8eb1eebd7f65
Takayuki Muranushi <muranushi@gmail.com>
2013-03-16 12:32:05 +0900

    git atomic

--- Heptacat/Utils.hs
+++ Heptacat/Utils.hs
@@ -1,6 +1,8 @@
 module Heptacat.Utils where
 
 import Data.String.Utils (split)
+import System.Posix.Directory (changeWorkingDirectory, getWorkingDirectory)
+import System.Process (runInteractiveProcess)
 
 gitUrl2Dir :: String -> FilePath
 gitUrl2Dir url = 
@@ -19,3 +21,23 @@ gitUrl2Dir url =
 defaultProjectFileName :: FilePath
 defaultProjectFileName = "project.yml"
 
+
+withWorkingDirectory :: FilePath -> IO a -> IO a
+withWorkingDirectory dir io = do
+  oldwd <- getWorkingDirectory
+  changeWorkingDirectory dir
+  ret <- io
+  changeWorkingDirectory oldwd
+  return ret
+
+gitCommitId :: IO String
+gitCommitId = do
+  (_,hOut,_,hProc) <- runInteractiveCommand "git log | head -n 1"
+  str <- hGetContents hOut
+  waitForProcess hProc
+  retturn $ words str !! 1
+
+gitAtomically :: IO a -> IO a
+gitAtomically io = do
+  oldCommitId <- gitCommitId             
+  
